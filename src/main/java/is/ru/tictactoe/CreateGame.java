8a5e8dc150edd9ab0bb2811c2234f5117bc75a46
Davíð <davidhaukurthorgilsson@Davis-MacBook-Pro.local>
2017-10-23 14:48:12 +0000

    Method displayGameBoard created. It displays the game bord

--- src/main/java/is/ru/tictactoe/CreateGame.java
+++ src/main/java/is/ru/tictactoe/CreateGame.java
@@ -1,6 +1,23 @@
-package main.java.is.ru.tictactoe;
+//package main.java.is.ru.tictactoe;
+package is.ru.tictactoe;
 
 public class CreateGame{
+        
+        
 
-   
+        // This method takes in a character array and
+        // displays the game board
+        static void displayGameBoard(char arr [][])
+	    {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    System.out.print(arr[row][col] + " ");
+
+                }
+                System.out.println();
+            }
+	    }
+
 }
--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,6 +1,14 @@
 package is.ru.tictactoe;
 
 public class TicTacToe{
-
+    public static void main(String[] args) {
+        
+        CreateGame newGame = new CreateGame();
+            // This is the array for the game board
+            char arr [][] = {{'1', '2', '3'},{ '4', '5', '6'},{ '7', '8', '9'}};  
+            
+            newGame.displayGameBoard(arr);
+   
+        }
    
 }
