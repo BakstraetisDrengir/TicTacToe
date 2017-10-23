ca84dfe330a35638f22e01bf78f5b2b80a10e123
asgeiro14 <asgeir14@ru.is>
2017-10-23 16:50:08 +0000

    Spark code for web

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,14 +1,28 @@
 package is.ru.tictactoe;
 
+import static spark.Spark.*;
+
 public class TicTacToe{
     public static void main(String[] args) {
         
-        CreateGame newGame = new CreateGame();
+        port(getHerokuPort());
+        get("/", (req, res) -> makeGame());
+        
+        }
+
+        static int getHerokuPort() {
+            ProcessBuilder psb = new ProcessBuilder();
+        if (psb.environment().get("PORT") != null) {
+            return Integer.parseInt(psb.environment().get("PORT"));
+        }
+        return 4567;
+        }
+
+        static void makeGame() {
+            CreateGame newGame = new CreateGame();
             // This is the array for the game board
             char arr [][] = {{'1', '2', '3'},{ '4', '5', '6'},{ '7', '8', '9'}};  
             
             newGame.displayGameBoard(arr);
-   
         }
-   
 }
