c0882de109d871ef66e69c4ae1a771e8309a806d
Davíð <davidhaukurthorgilsson@Davis-MacBook-Pro.local>
2017-10-27 11:26:29 +0000

    Index.html now only contains HTML. Script.js and Style.css created for javascript and css

---
+++ src/main/resources/public/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+  var form = $('form');
+  form.submit(function( event ) {
+    var number = $("#number").val();
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: 'number=' + number
+    }).done(function(result) {
+      $('#results').html(result).attr('class', 'alert alert-success');
+    }).fail(function() {
+      $('#results').html('Error, Illegal string...').attr('class', 'alert alert-danger');
+    });
+    event.preventDefault();
+  });
+
+
+  
+});
+
